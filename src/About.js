160e96a42944dcc85d78fc46eeb8563ffb766002
Alexander <alexander.jiw@gmail.com>
2025-03-26 17:07:50 +1100

    added skills bar

--- src/About.js
+++ src/About.js
@@ -19,8 +19,65 @@ const About = () => {
       <br />
       <br />
       <h1 className='about-me'>Skills</h1>
+      <div className='skills-container'>
+        <div className='skill-box'>
+          <span className='skill-title'>
+            HTML
+          </span>
+          <div className='skill-bar'>
+            <span className='skill-per' data-skill="HTML">
+              <span className='tooltip'>80%</span>
+            </span>
+          </div>
+        </div>
+        <div className='skill-box'>
+          <span className='skill-title'>
+            Java
+          </span>
+          <div className='skill-bar'>
+            <span className='skill-per' data-skill="JAVA">
+              <span className='tooltip'>70%</span>
+            </span>
+          </div>
+        </div>
+        <div className='skill-box'>
+          <span className='skill-title'>
+            C/C++
+          </span>
+          <div className='skill-bar'>
+            <span className='skill-per' data-skill="C">
+              <span className='tooltip'>60%</span>
+            </span>
+          </div>
+        </div>
+        <div className='skill-box'>
+          <span className='skill-title'>
+            Python
+          </span>
+          <div className='skill-bar'>
+            <span className='skill-per' data-skill="PYTHON">
+              <span className='tooltip'>80%</span>
+            </span>
+          </div>
+        </div>
+        
+      </div>
     </div>
   )
 }
 
+
+{/* <body>
+    <div class="container">
+        <div class="skill-box">
+            <span class="title">HTML</span>
+
+            <div class="skill-bar">
+                <span class="skill-per">
+                    <span class="tooltip">90%</span>
+                </span>
+            </div>
+        </div>
+    </div>
+</body> */}
 export default About
